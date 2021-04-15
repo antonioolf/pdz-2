74c71076a28abb4422f73ef76405265037bff3bf
Antônio Oliveira <antonio.filho128@gmail.com>
2021-04-14 22:39:56 -0300

    response improvement

--- src/main/kotlin/br/com/antoniooliveira/controller/PlanController.kt
+++ src/main/kotlin/br/com/antoniooliveira/controller/PlanController.kt
@@ -2,10 +2,10 @@ package br.com.antoniooliveira.controller
 
 import br.com.antoniooliveira.model.Plan
 import br.com.antoniooliveira.repository.PlanRepository
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
-import javax.inject.Inject
 
 @Controller("plan")
 class PlanController (private val planRepository: PlanRepository) {
@@ -16,7 +16,7 @@ class PlanController (private val planRepository: PlanRepository) {
     }
 
     @Post
-    fun save(plan: Plan): Plan {
-        return planRepository.save(plan)
+    fun save(plan: Plan): HttpResponse<Plan> {
+        return HttpResponse.created(planRepository.save(plan))
     }
 }
