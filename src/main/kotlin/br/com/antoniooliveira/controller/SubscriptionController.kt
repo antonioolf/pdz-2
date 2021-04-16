82503fc6340980d45f9dbbc7ce0dcd59958b7e1c
Antônio Oliveira <antonio.filho128@gmail.com>
2021-04-15 23:18:45 -0300

    Finish subscription crud

--- src/main/kotlin/br/com/antoniooliveira/controller/SubscriptionController.kt
+++ src/main/kotlin/br/com/antoniooliveira/controller/SubscriptionController.kt
@@ -1,4 +1,50 @@
 package br.com.antoniooliveira.controller
 
-class SubscriptionController {
+import br.com.antoniooliveira.model.Subscription
+import br.com.antoniooliveira.repository.SubscriptionRepository
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.*
+import java.util.*
+
+@Controller("subscription")
+class SubscriptionController (private val subscriptionRepository: SubscriptionRepository) {
+
+    @Post
+    fun create(subscription: Subscription): HttpResponse<Subscription> {
+        subscription.renewal_days = 30
+        subscription.active = true
+
+        val today = Date()
+        val calendar = Calendar.getInstance()
+        calendar.time = today
+        calendar.add(Calendar.DATE, 30)
+
+        subscription.start_subscription = today
+        subscription.end_subscription = calendar.time
+        subscription.next_renewal_date = calendar.time
+
+        return HttpResponse.created(subscriptionRepository.save(subscription))
+    }
+
+    @Get("/{id}")
+    fun read(id: Long): HttpResponse<Subscription> {
+        val subscription = subscriptionRepository.findById(id)
+
+        if (subscription.isEmpty) {
+            return HttpResponse.noContent()
+        }
+
+        return HttpResponse.created(subscription.get())
+    }
+
+    @Put
+    fun update(subscription: Subscription): HttpResponse<Subscription> {
+        return HttpResponse.created(subscriptionRepository.update(subscription))
+    }
+
+    @Delete
+    fun delete(id: Long): HttpResponse<Subscription>  {
+        subscriptionRepository.deleteById(id)
+        return HttpResponse.ok()
+    }
 }
---
+++ src/main/kotlin/br/com/antoniooliveira/repository/SubscriptionRepository.kt
@@ -0,0 +1,8 @@
+package br.com.antoniooliveira.repository
+
+import br.com.antoniooliveira.model.Subscription
+import io.micronaut.data.annotation.Repository
+import io.micronaut.data.jpa.repository.JpaRepository
+
+@Repository
+interface SubscriptionRepository: JpaRepository<Subscription, Long> {}
